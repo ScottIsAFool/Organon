f4df27400802debc71689ecd28a445f4fd898f34
Depechie <glenn.versweyveld@gmail.com>
2014-02-16 23:48:47 +0100

    Added empty ToggleImageControl

---
+++ Source/Organon.WP8.Controls/ToggleImage.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Organon.WP8.Controls
+{
+    public class ToggleImage : Control
+    {
+        public ToggleImage()
+        {
+            DefaultStyleKey = typeof (ToggleImage);
+        }
+    }
+}
