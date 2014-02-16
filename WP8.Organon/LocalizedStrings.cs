3481e960d7ad6d82812ef664ca2bb649d9930cef
Depechie <glenn.versweyveld@gmail.com>
2014-02-16 22:25:09 +0100

    Initial commit - demo apps

---
+++ WP8.Organon/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WP8.Organon.Resources;
+
+namespace WP8.Organon
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
