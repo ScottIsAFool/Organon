ff84e4852eb5881bc682c38300f0e93bee351615
skendrot <skendrot@hotmail.com>
2014-06-21 20:05:31 -0600

    Fix namespace

--- Source/Organon.Controls.Shared/PinterestPanel/PinterestPanel.cs
+++ Source/Organon.Controls.Shared/PinterestPanel/PinterestPanel.cs
@@ -3,7 +3,7 @@ using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-namespace Organon.Controls.PinterestPanl
+namespace Organon.Controls
 {
     public class PinterestPanel : Panel
     {
